cd50a6206debb5915e0dcb1d4999ef10e85940bc
AMW <73808213+AntonTech420@users.noreply.github.com>
2021-08-11 12:35:43 +1200

    First base movement working
    movement working

--- 2Deezy/Assets/Scripts/PlayerMovement.cs
+++ 2Deezy/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController2D controller;
+    public float RunSpeed = 40f;
+    float HorizontalMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
